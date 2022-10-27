464e2c2f02f17a3ed1e64dfea94288cdcd7a77c1
Baker Khoder <bakerkhoder878@gmail.com>
2022-10-27 06:48:48 +0300

    backend<>client feature:making the api wich add an item to the card

--- e-commerce-server/api/add_items_tocart.php
+++ e-commerce-server/api/add_items_tocart.php
@@ -1,14 +1,17 @@
 <?php
+
 include('connection.php');
 include_once "common.php";
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 $common = new Common();
 $response = [];
 if (
-    isset($_GET["client_id"], $_GET["product_id"])
+    isset($_POST["client_id"], $_POST["product_id"])
 ) {
-    $client_id = $_GET["client_id"];
-    $product_id = $_GET["product_id"];
+    $client_id = $_POST["client_id"];
+    $product_id = $_POST["product_id"];
 
     //check client existed
     if ($common->checkUserWithType($client_id, 'client')) {
